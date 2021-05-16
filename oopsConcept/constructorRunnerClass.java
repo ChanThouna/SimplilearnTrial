69d847aee57f628f8d99bb0682aa23a38ae5b159
ChanThouna <chanchanbina@gmail.com>
2021-05-16 05:02:43 +0000

    Test commit3

---
+++ oopsConcept/constructorRunnerClass.java
@@ -0,0 +1,11 @@
+package oopsConcept;
+
+public class constructorRunnerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassB obj = new ClassB("Chanchan",1000,"SimpliLearn");
+
+	}
+
+}
